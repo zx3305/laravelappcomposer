18634b22f201cf072bdc57fc84c84eca0140a625
mint <liumin104157@163.com>
2017-06-28 07:34:18 +0000

    调整目录结构

--- src/Business/OrderBusiness.php
+++ src/Business/OrderBusiness.php
@@ -9,9 +9,12 @@ use Paytest\Repository\Entity\CommodityEntity;
  * 订单业务逻辑
  */
 class OrderBusiness {
-	class add(OrderEntity $orderEntity, CommodityEntity $CommodityEntity){
+	/**
+	 * 下单业务逻辑
+	 */
+	public function add(OrderEntity $orderEntity, CommodityEntity $CommodityEntity){
 		$commodity = new CommodityBusiness();
-		$CommodityEntity = $commodity->sale($CommodityEntity, $orderEntity->stock, $orderEntity->money);			// 业务逻辑：扣减商品库存
+		$CommodityEntity = $commodity->sale($CommodityEntity, $orderEntity->stock, $orderEntity->money);	// 业务逻辑：扣减商品库存
 
 		return [$orderEntity, $CommodityEntity];	
 	}
