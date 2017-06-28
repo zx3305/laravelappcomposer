caa017275e252c97ecc528038b5ec3749800ab16
mint <liumin104157@163.com>
2017-06-28 08:14:19 +0000

    修改文件大小写

--- src/Interface/routes.php
+++ src/Interface/routes.php
@@ -1,5 +1,5 @@
 <?php
 Route::group(['middleware' => ['web'], 'namespace'=>'Paytest\Interface\Http'], function () {
-	Route::get('/test/getorder', 'OrderControl@getOrder');
-	Route::put('/test/add', 'OrderControl@add');
+	Route::any('/test/getorder', 'OrderController@getOrder');
+	Route::any('/test/add', 'OrderController@add');
 });
